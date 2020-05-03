f37b8b8fdd89c3ebb39ce935ec3727910a8769a9
chimnanishankar4 <63596198+chimnanishankar4@users.noreply.github.com>
2020-05-03 16:47:05 +0530

    Delete 2.py

--- 2.py
+++
@@ -1,5 +0,0 @@
-#2. Write a Python program to find the most common elements and their counts of a specified text.
-from collections import Counter
-list=[1,2,3,4,1,2,6,7,3,8,1]
-cnt=Counter(list)
-print(cnt.most_common())
