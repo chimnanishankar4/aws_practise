fee8d478a708fc5302f3ccfab1c59ed3019687f4
chimnanishankar4 <63596198+chimnanishankar4@users.noreply.github.com>
2020-05-03 16:47:23 +0530

    Delete 3.py

--- 3.py
+++
@@ -1,6 +0,0 @@
-#3. Write a Python program to create a new deque with three items and iterate over the deque's elements
-from collections import deque
-list=['a','b','c']
-deq=deque(list)
-for d in deq:
-    print(d)
